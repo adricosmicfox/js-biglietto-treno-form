824b24d101941892b9b960b2a409abc1550fc45d
adricosmicfox <adriiphone@gmail.com>
2022-08-27 18:29:49 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,29 +1,132 @@
 
-const form = document.getElementById("am-form");
+//inizializzazione variabile form (legare form a JS)
+const form = document.getElementById("am_form");
 
-form.addEventListener("submit", costoTotale);
+const costoKm = 0.21;
 
-function costoTotale() {
+//aggiunta funzione al bottone, al submit restituisce dati 
+form.addEventListener("submit", calcoloPrezzoFinale);
 
-    let risultato;
+//dichiarazione funzione submit per calcolo prezzoFinale
+function calcoloPrezzoFinale() {
 
-    let percorrenzaKm = document.getElementById("am_km").value;
-    console.log("am_km");
-    let etàViaggiatore = document.getElementById("am_age").value;
-    console.log("am_age");
+    let etaViaggiatore;
+    let kmViaggiatore;
+    let prezzo;
+    let nomeCognome;
 
-    risultato = percorrenzaKm * 0.21;
+    // inizializzazione varibaile nomeCognome (prendo il dato dal form)
+    nomeCognome = document.getElementById("am_nomecognome").value;
 
-    if (etàViaggiatore > 65)
-        risultato = risultato - (risultato * 0.40);
+    //inizializzazione variabile etaViaggiatore (prendo il dato dall'input)
+    etaViaggiatore = document.getElementById("am_age").value;
+    console.log(etaViaggiatore);
 
-    else if (etàViaggiatore < 18)
-        risultato = risultato - (risultato * 0.20);
+    //inizializzazione variabile kmViaggiatore (prendo il dato nell'input) 
+    kmViaggiatore = document.getElementById("am_km").value;
+    console.log(kmViaggiatore);
+
+    //calcolo del prezzo provvisorio senza sconto
+    prezzo = costoKm * kmViaggiatore;
+    console.log(prezzo);
 
 
-    document.getElementById("am_cost").innerHTML = risultato.toFixed(2);
+    // gestione casi sconto
+    if (isNaN(kmViaggiatore) || isNaN(etaViaggiatore)) {
+        alert("Dati inseriti in modo errato");
+    }
+
+    if (etaViaggiatore > 65) {
+        prezzo = prezzo - prezzo * 0.4;
+    }
+
+    else if (etaViaggiatore < 18) {
+        prezzo -= prezzo * 0.2;
+    }
+
+    // stampo nell'html dati seguenti
+    document.getElementById("am_cellanomecognome").innerHTML = nomeCognome;
+
+    document.getElementById("am_cellacarrozza").innerHTML = Math.floor(Math.random() * 12);
+
+    document.getElementById("am_cellacodicecp").innerHTML = Math.floor(Math.random() * 100000);
+
+    document.getElementById("am_cost").innerHTML = "€ " + prezzo.toFixed(2);
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.getElementById("am-form");
+
+// form.addEventListener("submit", costoTotale);
+
+// function costoTotale() {
+
+//     let risultato;
+
+//     let percorrenzaKm = document.getElementById("am_km").value;
+//     console.log("am_km");
+//     let etàViaggiatore = document.getElementById("am_age").value;
+//     console.log("am_age");
+
+//     risultato = percorrenzaKm * 0.21;
+
+//     if (etàViaggiatore > 65)
+//         risultato = risultato - (risultato * 0.40);
+
+//     else if (etàViaggiatore < 18)
+//         risultato = risultato - (risultato * 0.20);
+
+
+//     document.getElementById("am_cost").innerHTML = risultato.toFixed(2);
+
+// }
 
 
 
